79521f5b05c34fd94f026088ac39764ebbbb1f33
Johnny Hicks <johnnydhicks@gmail.com>
2018-12-14 09:16:43 -0700

    Remove comment

--- CruiseTheNews/View Controllers/CruiseTheNewsTableViewController.swift
+++ CruiseTheNews/View Controllers/CruiseTheNewsTableViewController.swift
@@ -31,7 +31,6 @@ class CruiseTheNewsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return newsArticles.count
     }
 
