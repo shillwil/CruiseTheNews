fdea2afc9a76f68356e16abd57d97c0dc0f78b38
Johnny Hicks <johnnydhicks@gmail.com>
2018-12-13 20:16:56 -0700

    Set up main wireframe

---
+++ CruiseTheNews/View Controllers/CruiseTheNewsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CruiseTheNewsDetailViewController.swift
+//  CruiseTheNews
+//
+//  Created by Johnny Hicks on 12/13/18.
+//  Copyright © 2018 Johnny Hicks. All rights reserved.
+//
+
+import UIKit
+
+class CruiseTheNewsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
